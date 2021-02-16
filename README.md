cd9d82118e86dbe293de5f99faa63ff3442e20fb
Fabian Weller <7693897+Madokamiiiiii@users.noreply.github.com>
2021-02-16 15:36:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# LaTeX to Matlab LaTeX Converter
+
+A simple tool which converts Latex to Matlab Latex (at first only matrices).
+
+This project uses the Flutter framework.
---
+++ android/app/src/main/kotlin/de/fabianweller/latextomatlabconvert/latex_to_matlab_converter/MainActivity.kt
@@ -0,0 +1,6 @@
+package de.fabianweller.latextomatlabconvert.latex_to_matlab_converter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
